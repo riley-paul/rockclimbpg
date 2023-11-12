48c6fe9b9bc21ee6cc969096463eb37d4a48e32f
Riley Paul <rileypaul96@gmail.com>
2023-11-11 21:00:17 -0800

    get map working

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { MapContainer, TileLayer, useMap, Marker as MarkerObj, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet";
+import L from "leaflet";
 
 interface TileLayer {
   url: string;
@@ -17,14 +18,12 @@ const tileLayers: Record<string, TileLayer> = {
   topo: {
     name: "Topographic",
     url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}",
-    attribution:
-      "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community",
+    attribution: "Tiles &copy; Esri",
   },
   image: {
     name: "Imagery",
     url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-    attribution:
-      "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+    attribution: "Tiles &copy; Esri",
   },
 };
 
@@ -36,25 +35,32 @@ export interface MarkerObj {
 interface Props {
   tile: keyof typeof tileLayers;
   markers: MarkerObj[];
+  height?: number;
 }
 
 export const Map: React.FC<Props> = (props) => {
   const { tile, markers } = props;
   const tileLayer = tileLayers[tile];
 
+  const centerLat =
+    markers.reduce((acc, val) => acc + val.position[0], 0) / markers.length;
+  const centerLon =
+    markers.reduce((acc, val) => acc + val.position[1], 0) / markers.length;
+
   return (
     <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
+      center={[53.91745588242724, -122.74997120715773]}
+      zoom={8}
       scrollWheelZoom={false}
-      className="h-[300px] rounded-md"
+      style={{ height: props.height ?? 300 }}
+      className="rounded-md"
     >
       <TileLayer attribution={tileLayer.attribution} url={tileLayer.url} />
-      <MarkerObj position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </MarkerObj>
+      {markers.map((marker) => (
+        <Marker position={marker.position}>
+          <Tooltip>{marker.popup}</Tooltip>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
