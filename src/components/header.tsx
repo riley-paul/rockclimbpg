05d4ae358c09e03769c25f5334c04ac1b9087258
Riley Paul <rileypaul96@gmail.com>
2025-09-02 12:31:09 -0600

    add contact page and start on nav links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,50 @@
 import React from "react";
 import Logo from "./logo";
+import { Button, Link } from "@radix-ui/themes";
+
+type LinkInfo = {
+  url: string;
+  label: string;
+  isActive: (pathname: string) => boolean;
+  children?: LinkInfo[];
+};
+
+const linkInfos: LinkInfo[] = [
+  {
+    url: "/",
+    label: "Home",
+    isActive: (pathname) => pathname === "/",
+  },
+  {
+    url: "/resources",
+    label: "Resources",
+    isActive: (pathname) => pathname.startsWith("/resources"),
+  },
+  {
+    url: "/contact",
+    label: "Contact",
+    isActive: (pathname) => pathname === "/contact",
+  },
+];
+
+const NavLink: React.FC<{ link: LinkInfo }> = ({
+  link: { url, label, isActive },
+}) => {
+  return (
+    <Link size="2" href={url}>
+      {label}
+    </Link>
+  );
+};
 
 const Header: React.FC = () => {
   return (
     <header className="container2 flex h-16 items-center justify-between">
       <Logo />
-      <section className="flex items-center gap-2">
+      <section className="flex items-center gap-4">
+        {linkInfos.map((link) => (
+          <NavLink key={link.url} link={link} />
+        ))}
       </section>
     </header>
   );
