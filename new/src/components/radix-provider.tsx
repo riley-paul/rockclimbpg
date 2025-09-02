e3039f0decc9e4be9287826ca2ce6e6ac02f21d0
Riley Paul <rileypaul96@gmail.com>
2025-09-02 09:46:39 -0600

    add theming support

--- new/src/components/radix-provider.tsx
+++ new/src/components/radix-provider.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Theme, type ThemeProps } from "@radix-ui/themes";
+import { useAppearance } from "@/lib/use-theme";
 
 const RadixProvider: React.FC<ThemeProps> = (props) => {
-  return <Theme accentColor="grass" {...props} />;
+  const appearance = useAppearance();
+  return <Theme accentColor="grass" appearance={appearance} {...props} />;
 };
 
 export default RadixProvider;
