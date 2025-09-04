7746ef1af105bc470cf034dcd0ecf41a58cc7c3d
Riley Paul <rileypaul96@gmail.com>
2025-09-03 20:36:21 -0600

    use css rather than react for responsive menus

--- src/components/nav-menu.tsx
+++ src/components/nav-menu.tsx
@@ -1,18 +1,13 @@
 import type { LinkInfo } from "@/lib/types";
-import React, { useState } from "react";
+import React from "react";
 
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import * as Dialog from "@radix-ui/react-dialog";
 
-import {
-  ChevronDownIcon,
-  ChevronRightIcon,
-  MenuIcon,
-  XIcon,
-} from "lucide-react";
+import { ChevronDownIcon, MenuIcon, XIcon } from "lucide-react";
 import { Button, Card, IconButton, Link, Portal } from "@radix-ui/themes";
 import { getIsActiveUrl } from "@/lib/utils";
-import { ACCENT_COLOR, MOBILE_MEDIA_QUERY } from "@/lib/constants";
+import { ACCENT_COLOR } from "@/lib/constants";
 import { useMediaQuery } from "usehooks-ts";
 import RadixProvider from "./radix-provider";
 
@@ -20,7 +15,7 @@ type Props = { pathname: string; linkInfos: LinkInfo[] };
 
 const DesktopNavMenu: React.FC<Props> = ({ pathname, linkInfos }) => {
   return (
-    <NavigationMenu.Root className="relative">
+    <NavigationMenu.Root className="relative hidden sm:block">
       <NavigationMenu.List className="flex list-none items-baseline gap-6">
         {linkInfos.map((linkInfo) => {
           const isActive = getIsActiveUrl(linkInfo.url, pathname);
@@ -101,75 +96,79 @@ const DesktopNavMenu: React.FC<Props> = ({ pathname, linkInfos }) => {
 
 const MobileNavMenu: React.FC<Props> = ({ linkInfos, pathname }) => {
   return (
-    <Dialog.Root>
-      <Dialog.Trigger asChild>
-        <IconButton variant="surface">
-          <MenuIcon className="size-4" />
-        </IconButton>
-      </Dialog.Trigger>
-      <Portal>
-        <RadixProvider>
-          <Dialog.Overlay className="fixed inset-0" />
-          <Dialog.Content className="bg-background data-[state=open]:animate-in data-[state=closed]:animate-out fade-in fade-out fixed inset-0 top-1 overflow-auto outline-none">
-            <header className="container2 absolute inset-0 top-1 flex h-20 items-center justify-end">
-              <Dialog.Close asChild>
-                <IconButton variant="soft" color="gray" radius="full">
-                  <XIcon className="size-4" />
-                </IconButton>
-              </Dialog.Close>
-            </header>
-            <article className="container2 py-6">
-              <ul className="grid gap-2">
-                {linkInfos.map((linkInfo) => {
-                  const isActive = getIsActiveUrl(linkInfo.url, pathname);
-                  return (
-                    <li className="">
-                      <Button
-                        variant="ghost"
-                        asChild
-                        className="flex-1 justify-start! text-left!"
-                        color={isActive ? ACCENT_COLOR : "gray"}
-                      >
-                        <a href={linkInfo.url}>{linkInfo.label}</a>
-                      </Button>
-                      {linkInfo.children && (
-                        <ul className="border-gray-6 mt-1 ml-1 grid gap-1 border-l pl-4">
-                          {linkInfo.children.map((child) => {
-                            const isActive = getIsActiveUrl(
-                              child.url,
-                              pathname,
-                            );
-                            return (
-                              <li className="w-full" key={child.url}>
-                                <Button
-                                  asChild
-                                  variant="ghost"
-                                  color={isActive ? ACCENT_COLOR : "gray"}
-                                >
-                                  <a href={child.url}>{child.label}</a>
-                                </Button>
-                              </li>
-                            );
-                          })}
-                        </ul>
-                      )}
-                    </li>
-                  );
-                })}
-              </ul>
-            </article>
-          </Dialog.Content>
-        </RadixProvider>
-      </Portal>
-    </Dialog.Root>
+    <div className="block sm:hidden">
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <IconButton variant="surface" aria-label="Menu">
+            <MenuIcon className="size-4" />
+          </IconButton>
+        </Dialog.Trigger>
+        <Portal>
+          <RadixProvider>
+            <Dialog.Overlay className="fixed inset-0" />
+            <Dialog.Content className="bg-background data-[state=open]:animate-in data-[state=closed]:animate-out fade-in fade-out fixed inset-0 top-1 overflow-auto outline-none">
+              <header className="container2 absolute inset-0 top-1 flex h-20 items-center justify-end">
+                <Dialog.Close asChild>
+                  <IconButton variant="soft" color="gray" radius="full">
+                    <XIcon className="size-4" />
+                  </IconButton>
+                </Dialog.Close>
+              </header>
+              <article className="container2 py-6">
+                <ul className="grid gap-2">
+                  {linkInfos.map((linkInfo) => {
+                    const isActive = getIsActiveUrl(linkInfo.url, pathname);
+                    return (
+                      <li className="">
+                        <Button
+                          variant="ghost"
+                          asChild
+                          className="flex-1 justify-start! text-left!"
+                          color={isActive ? ACCENT_COLOR : "gray"}
+                        >
+                          <a href={linkInfo.url}>{linkInfo.label}</a>
+                        </Button>
+                        {linkInfo.children && (
+                          <ul className="border-gray-6 mt-1 ml-1 grid gap-1 border-l pl-4">
+                            {linkInfo.children.map((child) => {
+                              const isActive = getIsActiveUrl(
+                                child.url,
+                                pathname,
+                              );
+                              return (
+                                <li className="w-full" key={child.url}>
+                                  <Button
+                                    asChild
+                                    variant="ghost"
+                                    color={isActive ? ACCENT_COLOR : "gray"}
+                                  >
+                                    <a href={child.url}>{child.label}</a>
+                                  </Button>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        )}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </article>
+            </Dialog.Content>
+          </RadixProvider>
+        </Portal>
+      </Dialog.Root>
+    </div>
   );
 };
 
 const NavMenu: React.FC<Props> = (props) => {
-  const isMobile = useMediaQuery(MOBILE_MEDIA_QUERY);
-
-  if (isMobile) return <MobileNavMenu {...props} />;
-  return <DesktopNavMenu {...props} />;
+  return (
+    <>
+      <DesktopNavMenu {...props} />
+      <MobileNavMenu {...props} />
+    </>
+  );
 };
 
 export default NavMenu;
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,1 @@
-export const MOBILE_MEDIA_QUERY = "(max-width: 768px)";
 export const ACCENT_COLOR = "grass";
